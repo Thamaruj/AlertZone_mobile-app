1495e7822ad185ea0cdf27e641681d2c7281f6d7
Thamaruj <thamarujalthotage1@gmail.com>
2026-04-01 14:51:41 +0530

    Adjust toast offset to avoid Dynamic Island overlap on iOS

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,20 @@ import { Stack } from "expo-router";
 import "../global.css";
 import Toast from "react-native-toast-message"; // <-- import Toast
 import { toastConfig } from "../config/toastConfig"; // make sure this matches the exported name
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
+    const insets = useSafeAreaInsets();
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }} />
-      <Toast config={toastConfig} />
+        <Stack screenOptions={{ headerShown: false }} />
+          <Toast
+            config={toastConfig}
+            topOffset={
+              Platform.OS === "ios" ? insets.top + 0 : 50
+            }
+          />
     </>
   );
 }
